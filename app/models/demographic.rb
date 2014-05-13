36e2797021489a47b21f0331ed48a55a9b946139
garyhsieh <garyhsieh@gmail.com>
2014-05-13 09:46:07 -0700

    update demographics

--- app/models/demographic.rb
+++ app/models/demographic.rb
@@ -1,5 +1,5 @@
 class Demographic < ActiveRecord::Base
   belongs_to :participant
-  validates_presence_of :age, :state, :site_01, :topic_01, :topic_02, :topic_03, :topic_04, :topic_05, :topic_06
+  validates_presence_of :age, :state, :site_01, :topic_01, :topic_02, :topic_03, :topic_04, :topic_05, :topic_06, :topic_07, :topic_08, :topic_09
 
 end
